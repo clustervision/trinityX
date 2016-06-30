4701ebb233eb2a402e3ece685e61533e2a453f13
JF Le Fillâtre <jf.lefillatre@clustervision.com>
2016-06-30 11:00:03 +0200

    Add shared environment modules support

---
+++ controller-post/environment-modules.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Environment modules setup
+
+source /etc/trinity.sh
+
+echo "Creating the shared modules directories"
+
+# Contents of the modulefiles directory:
+# /trinity/shared/
+# `-- modulefiles
+#     |-- CV-advanced       advanced CV modules, not available by default
+#     ¦-- CV-standard       standard CV modules, available by default
+#     |-- local             site-local modules, available by default
+#      -- modulegroups      modulefiles to load groups (advanced, local, etc)
+
+mkdir -pv "${TRIX_ROOT}/shared/modulefiles"
+mkdir -pv "${TRIX_ROOT}/shared/modulefiles/modulegroups"
+mkdir -pv "${TRIX_ROOT}/shared/modulefiles/CV-standard"
+mkdir -pv "${TRIX_ROOT}/shared/modulefiles/CV-advanced"
+mkdir -pv "${TRIX_ROOT}/shared/modulefiles/local"
+
+echo "Adding the group path to the default configuration"
+
+echo "${TRIX_ROOT}/shared/modulefiles/modulegroups" | tee -a /usr/share/Modules/init/.modulespath
+echo "${TRIX_ROOT}/shared/modulefiles/CV-standard" | tee -a /usr/share/Modules/init/.modulespath
+echo "${TRIX_ROOT}/shared/modulefiles/local" | tee -a /usr/share/Modules/init/.modulespath
+
+echo "Adding the group modulefiles"
+
+cp -v "${POST_FILEDIR}/CV-advanced" "${TRIX_ROOT}/shared/modulefiles/modulegroups"
+
+echo "Adjusting the trinityX installation path"
+
+sed -i 's#/TRIX_ROOT#'"$TRIX_ROOT"'#g' "${TRIX_ROOT}/shared/modulefiles/modulegroups/"*
+
