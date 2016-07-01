333e167144923d9e414454ab79b39a35a54e9a27
JF Le Fillâtre <jf.lefillatre@clustervision.com>
2016-07-01 14:29:24 +0200

    Updating the SSSD config to use the configuration environment

--- controller-post/sssd.sh
+++ controller-post/sssd.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-# Initialize configuration file
-sed "s,{{ controller }},$CONTROLLER_HOSTNAME," sssd/sssd.conf > /etc/sssd/sssd.conf
+source /etc/trinity.sh
+
+
+echo '*** Creating the SSSD configuration file'
+
+sed "s,{{ controller }},${TRIX_CTRL_HOSTNAME}," "${POST_FILEDIR}"/sssd.conf > /etc/sssd/sssd.conf
 chmod 600 /etc/sssd/sssd.conf
+
+
+echo '*** Enabling and starting the service'
 
 systemctl enable sssd
 systemctl start sssd
