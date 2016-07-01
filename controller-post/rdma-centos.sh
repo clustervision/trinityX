142016addadb8e4c76e909dd70f0df8ce1e8c575
JF Le Fillâtre <jf.lefillatre@clustervision.com>
2016-07-01 12:13:29 +0200

    Add post script for default kernel RDMA implementation

---
+++ controller-post/rdma-centos.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Enable RDMA on interfaces that support it, with the default package included
+# in the CentOS distribution.
+
+# You will want to disable this post script if you're using a HW-specific RDMA
+# configuration.
+
+echo '*** Enabling and starting the RDMA service:'
+
+systemctl enable rdma
+systemctl start rdma
+
