9584325e779a81ca278059649e39a82b3a7f6871
JF Le Fillâtre <jf.lefillatre@clustervision.com>
2016-07-04 20:06:54 +0200

    Cleanup part 4: refactor the main script
    - used shared functions for pretty outputs
    
    - loop over configuration files instead of post scripts
    
    - try to do as much magic as possible to figure out in which directory are the scripts. Chances of breakage due to an unforeseen weird input: 100%.

--- configuration/configure.sh
+++ configuration/configure.sh
@@ -1,112 +1,158 @@
 #!/bin/bash
 
-# trinityX
-# Optional post-installation scripts
+# Trinity X configuration tool
 
-
-################################################################################
-## List of post-installation scripts to run
-
-# Comment out the ones that you don't want, or add yours in the list.
-# Warning: there might be hidden dependancies between existing scripts, so don't
-#          go around changing the order just for the fun of it!
-
-# Known rules:
-# - local-repo goes first
-# - base-packages goes second
-# - packages that require post-install scripts, are optional or come from an
-#   extra repo (which are optional by default) go into their own post files. The
-#   rest goes into base-packages.pkglist
-
-
-POSTLIST=( \
-           local-repo \
-           base-packages \
-           additional-repos \
-           yum-update \
-           firewalld \
-           environment-modules \
-           rdma-centos \
-           nfs-shared \
-           nfs-home \
-           openldap \
-           sssd \
-         )
+# USAGE:
+# configure.sh config_file
+#
+# The configuration file is a valid shell script that contains environment
+# variables. It will be sourced later, and then various post-scripts will be
+# able to access those variables for their configuration.
+#
+# At least one configuration file is required. If more than one is specified,
+# they will run in that order. It is up to the user to break the installation if
+# something goes wrong; the scripts will run regardless of the success of
+# failure of previous scripts.
 
 
 
 ################################################################################
-## Let's go
+##
+## NO ACCESS TO THE SHARED FUNCTIONS YET
+##
+################################################################################
 
-# A bit ugly, but errors in the output are identified much more quickly:
-function myecho {
-	echo -e "\n################################################################################"
-        echo "####  $@"
-	echo
-}
 
-# Set up a few environment variables that we will need later
-MYFNAME="$(readlink -f "$0")"
-MYPATH="$(dirname "$MYFNAME")"
+# Right number of arguments?
 
-#---------------------------------------
+if ! (( $# )) ; then
+    echo "Syntax: $0 config_file [config_file ...]" >&2
+    exit 1
+fi
 
-# Treat parameters are script names and override the built-in list:
-(( $# > 0 )) && POSTLIST=("$@")
-
-echo "#### List of post scripts to run:"
-
-echo "${POSTLIST[@]}" | tr ' ' '\n'
-echo
 
 #---------------------------------------
+
+# Set up some environment variables and load the common file, which should
+# reside in the same directory as the configuration script.
+
+MYPATH="$(dirname "$(readlink -f "$0")")"
 
 export POST_TOPDIR="$(dirname "${MYPATH}")"
 export POST_COMMON="${MYPATH}/common_functions.sh"
 
-for i in "${POSTLIST[@]}" ; do
-	
-	bname="$(basename "$i" .sh)"
-	echo "####  Running post script: $bname"
-	export POST_PKGLIST="${MYPATH}/${bname}.pkglist"
-	export POST_SCRIPT="${MYPATH}/${bname}.sh"
-	export POST_FILEDIR="${MYPATH}/${bname}"
-	ret=0
-	
-	# Start with installing the packages if we have a list
-	if [[ -r "$POST_PKGLIST" ]] ; then
-		yum -y install $(grep -v '^#\|^$' "$POST_PKGLIST")
-		ret=$?
-	else
-		echo "## No package file found: $POST_PKGLIST"
-		echo
-	fi
-	
-	# Take a break if the installation didn't go right
-	if (( $ret )) ; then
-		echo
-		echo "## Error during package installation: $POST_PKGLIST"
-		read -p "   Press Enter continue."
-	fi
-	
-	# Then run the script if we have one
-	if [[ -r "$POST_SCRIPT" ]] ; then
-		bash "$POST_SCRIPT"
-		ret=$?
-	else
-		echo "## No post script found: $POST_SCRIPT"
-		echo
-	fi
-	
-	# Take a break if the script returned an error code
-	if (( $ret )) ; then
-		echo
-		echo "## Error during post script: $POST_SCRIPT"
-		read -p "   Press Enter continue."
-	fi
-	
-	unset POST_PKGLIST POST_SCRIPT POST_FILEDIR
-done
+if ! source "$POST_COMMON" ; then
+    echo "ERROR: cannot source common file \"$POST_COMMON\"" >&2
+    exit 1
+fi
 
-unset POST_TOPDIR POST_COMMON
+
+
+################################################################################
+##
+## SHARED FUNCTIONS ACCESSIBLE
+##
+################################################################################
+
+# Go through a single post script
+
+# Syntax: run_one_script script_name
+
+# All the other parameters are environment variables.
+
+function run_one_script {
+    
+    echo_progress "Running post script: $1"
+    
+    export POST_PKGLIST="${POSTDIR}/${1}.pkglist"
+    export POST_SCRIPT="${POSTDIR}/${1}.sh"
+    export POST_FILEDIR="${POSTDIR}/${1}"
+    ret=0
+    
+    # Start with installing the packages if we have a list
+    if [[ -r "$POST_PKGLIST" ]] ; then
+        yum -y install $(grep -v '^#\|^$' "$POST_PKGLIST")
+        ret=$?
+    else
+        echo_warn "No package file found: $POST_PKGLIST"
+    fi
+    
+    # Take a break if the installation didn't go right
+    if (( $ret )) ; then
+        echo_error_wait "Error during package installation: $POST_PKGLIST"
+    fi
+    
+    # Then run the script if we have one
+    if [[ -r "$POST_SCRIPT" ]] ; then
+        bash "$POST_SCRIPT"
+        ret=$?
+    else
+        echo_warn "No post script found: $POST_SCRIPT"
+    fi
+    
+    # Take a break if the script returned an error code
+    if (( $ret )) ; then
+        echo_error_wait "Error during post script: $POST_SCRIPT"
+    fi
+    
+    unset POST_PKGLIST POST_SCRIPT POST_FILEDIR
+}
+
+
+#---------------------------------------
+
+# Main loop to run for each configuration file passed in parameter
+
+# Syntax: apply_config configuration_file
+
+function apply_config {
+    
+    CONFFILE="$(readlink -e "$1")"
+    CONFDIR="$(dirname "$CONFFILE")"
+
+    if ! source "$CONFFILE" ; then
+        echo_error_wait "Cannot source configuration file: $CONFFILE"
+        return
+    fi
+    
+    # Deal with variations in the POSTDIR values:
+    # - defined and absolute -> nothing to do
+    # - defined and relative -> prepend CONFDIR
+    # - undefined -> same as the conf file without extension
+    
+    if [[ "$POSTDIR" ]] ; then
+        if ! [[ "$POSTDIR" =~ ^/.* ]] ; then
+            POSTDIR="${CONFDIR}/${POSTDIR}"
+        fi
+    else
+        POSTDIR="$(basename "$CONFFILE" .cfg)"
+    fi
+    
+    POSTDIR=$(readlink -f "$POSTDIR")
+
+    if (( $? )) ; then
+        echo_error_wait "Configuration directory doesn't exist: $POSTDIR"
+        return
+    fi
+    
+    #echo_info "Configuration directory: $POSTDIR"
+    #echo_info "List of post scripts: ${POSTLIST[@]}"
+    
+    # Alright, so at that point we have our config file loaded, and we know
+    # where the directory is, we can loop over the list of post scripts.
+    
+    for post in "${POSTLIST[@]}" ; do
+        run_one_script "$post"
+    done
+}
+
+
+#---------------------------------------
+
+# And finally, loop over the parameters
+
+for cfg in "$@" ; do
+    echo_header "CONFIGURATION FILE: $cfg"
+    apply_config "$cfg"
+done
 
