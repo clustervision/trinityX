cac5493e3f8a83712818e387a10aa2a98b7c8243
JF Le Fillâtre <jf.lefillatre@clustervision.com>
2016-07-13 11:24:51 +0200

    Merge the two NFS configuration files

--- configuration/controller/nfs-shared.sh
+++ configuration/controller/nfs-shared.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
 source /etc/trinity.sh
-
-# Enable the NFS server and export the shared directory
-
 source "$POST_CONFIG"
 
-OPTS="${NFS_SHARED_OPTS:-ro,no_root_squash}"
+# Enable the NFS server and exports
+
+SHARED_OPTS="${NFS_SHARED_OPTS:-ro,no_root_squash}"
 
 
 echo_info 'Adding the shared export'
 
-append_line "${TRIX_ROOT}/shared (${OPTS})" /etc/exports
+append_line "${TRIX_ROOT}/shared (${SHARED_OPTS})" /etc/exports
+store_variable /etc/trinity.sh NFS_SHARED "${TRIX_ROOT}/shared"
+
+
+if [[ "$NFS_HOME_OPTS" ]] ; then
+    echo_info 'Adding the /home export'
+    append_line "/home (${NFS_HOME_OPTS})" /etc/exports
+fi
+
+
+if [[ "$NFS_HOME_RPCCOUNT" ]] ; then
+    echo_info 'Adjusting the number of threads for the NFS server'
+    sed -i 's/[# ]*\(RPCNFSDCOUNT=\).*/\1'"${NFS_HOME_RPCCOUNT}"'/g' /etc/sysconfig/nfs
+fi
 
 
 echo_info 'Enabling and starting the NFS server'
