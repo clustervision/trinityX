d7b7f1606aa26701b14558c643dea7d200ebbc7f
Dmitry Chirikov <dmitry@chirikov.ru>
2016-07-19 12:38:07 +0200

    Useradd requires username to be specified. Apparently.

--- configuration/controller/slurm-pre.sh
+++ configuration/controller/slurm-pre.sh
@@ -12,7 +12,7 @@ if [ "x${MUNGE_GROUP_ID}" = "x" ]; then
     store_variable "${TRIX_SHFILE}" MUNGE_GROUP_ID $(getent group | awk -F\: '$1=="munge"{print $3}')
 fi
 if [ "x${MUNGE_USER_ID}" = "x" ]; then 
-    useradd -r -g munge -d /var/run/munge -s /sbin/nologin
+    useradd -r -g munge -d /var/run/munge -s /sbin/nologin munge
     store_variable "${TRIX_SHFILE}" MUNGE_USER_ID $(id -u munge)
 fi
 if [ "x${SLURM_GROUP_ID}" = "x" ]; then 
@@ -20,7 +20,7 @@ if [ "x${SLURM_GROUP_ID}" = "x" ]; then
     store_variable "${TRIX_SHFILE}" SLURM_GROUP_ID $(getent group | awk -F\: '$1=="slurm"{print $3}')
 fi
 if [ "x${SLURM_USER_ID}" = "x" ]; then
-    useradd -r -g slurm -d /var/log/slurm  -s /sbin/nologin
+    useradd -r -g slurm -d /var/log/slurm  -s /sbin/nologin slurm
     store_variable "${TRIX_SHFILE}" SLURM_USER_ID $(id -u slurm)
 fi
 
