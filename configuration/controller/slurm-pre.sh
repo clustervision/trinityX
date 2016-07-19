bbf80ea75f386eb06f8bce709d56e62988258f2b
Dmitry Chirikov <dmitry@chirikov.ru>
2016-07-19 12:36:29 +0200

    slurm/munge user creation

--- configuration/controller/slurm-pre.sh
+++ configuration/controller/slurm-pre.sh
@@ -5,11 +5,24 @@ source /etc/trinity.sh
 
 echo_info "Creating Slurm and Munge users"
 
-useradd munge -U
-useradd slurm -U
 
-store_variable "${TRIX_ROOT}/trinity.sh" MUNGE_USER_ID $(id -u munge)
-store_variable "${TRIX_ROOT}/trinity.sh" SLURM_USER_ID $(id -u slurm)
+
+if [ "x${MUNGE_GROUP_ID}" = "x" ]; then 
+    groupadd -r munge
+    store_variable "${TRIX_SHFILE}" MUNGE_GROUP_ID $(getent group | awk -F\: '$1=="munge"{print $3}')
+fi
+if [ "x${MUNGE_USER_ID}" = "x" ]; then 
+    useradd -r -g munge -d /var/run/munge -s /sbin/nologin
+    store_variable "${TRIX_SHFILE}" MUNGE_USER_ID $(id -u munge)
+fi
+if [ "x${SLURM_GROUP_ID}" = "x" ]; then 
+    groupadd -r slurm
+    store_variable "${TRIX_SHFILE}" SLURM_GROUP_ID $(getent group | awk -F\: '$1=="slurm"{print $3}')
+fi
+if [ "x${SLURM_USER_ID}" = "x" ]; then
+    useradd -r -g slurm -d /var/log/slurm  -s /sbin/nologin
+    store_variable "${TRIX_SHFILE}" SLURM_USER_ID $(id -u slurm)
+fi
 
 mkdir -p /var/log/slurm
 chown slurm:slurm /var/log/slurm
