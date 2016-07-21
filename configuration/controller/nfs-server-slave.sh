fc380707543649d0c24b18386becb93ceffd7ac7
Dmitry Chirikov <dmitry@chirikov.ru>
2016-07-21 16:53:35 +0200

    nfs-server-slave files

---
+++ configuration/controller/nfs-server-slave.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source /etc/trinity.sh
+source "$POST_CONFIG"
+
+
+# Enable the NFS server and exports
+
+SHARED_OPTS="${NFS_SHARED_OPTS:-ro,no_root_squash}"
+
+
+echo_info 'Adding the shared export'
+
+append_line "${TRIX_SHARED} (${SHARED_OPTS})" /etc/exports
+
+
+if [[ "$NFS_HOME_OPTS" ]] ; then
+    echo_info 'Adding the /home export'
+    append_line "${TRIX_HOME} (${NFS_HOME_OPTS})" /etc/exports
+    store_variable /etc/trinity.sh HOME_ON_NFS 1
+fi
+
+
+if [[ "$NFS_HOME_RPCCOUNT" ]] ; then
+    echo_info 'Adjusting the number of threads for the NFS server'
+    sed -i 's/[# ]*\(RPCNFSDCOUNT=\).*/\1'"${NFS_HOME_RPCCOUNT}"'/g' /etc/sysconfig/nfs
+fi
