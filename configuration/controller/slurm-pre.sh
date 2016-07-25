ba90f9adfad09edc13c850c46924f9527261ff3a
Dmitry Chirikov <dmitry@chirikov.ru>
2016-07-26 01:32:58 +0200

    Slurm/mongo uid/gid should be pre-defined

--- configuration/controller/slurm-pre.sh
+++ configuration/controller/slurm-pre.sh
@@ -3,8 +3,6 @@ set -e
 
 display_var MUNGE_{GROUP,USER}_ID SLURM_{GROUP,USER}_ID
 
-# commit f786d7ec58b9fc0aad1649f6cdd6aa3ed2bec153 causes multiple checks
-
 echo_info "Creating Slurm and Munge users"
 
 groupadd -r ${MUNGE_GROUP_ID:+"-g $MUNGE_GROUP_ID"} munge 
@@ -18,18 +16,6 @@ store_variable "${TRIX_SHFILE}" SLURM_GROUP_ID $(getent group | awk -F\: '$1=="s
 
 useradd -r ${SLURM_USER_ID:+"-u $SLURM_USER_ID"} -g slurm -d /var/log/slurm  -s /sbin/nologin slurm
 store_variable "${TRIX_SHFILE}" SLURM_USER_ID $(id -u slurm)
-
-# section for slave.
-source /etc/trinity.sh
-getent group | grep -q munge || ( \
-    groupadd -g ${MUNGE_GROUP_ID} munge && \
-    useradd -r -g munge -d /var/run/munge -u ${MUNGE_USER_ID} -s /sbin/nologin munge \
-)
-getent group | grep -q slurm || ( \
-    groupadd -g ${SLURM_GROUP_ID} slurm && \
-    useradd -r -g slurm -d /var/log/slurm -u ${SLURM_USER_ID} -s /sbin/nologin slurm \
-)
-
 
 mkdir -p /var/log/slurm
 chown slurm:slurm /var/log/slurm
