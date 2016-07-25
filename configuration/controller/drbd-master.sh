6ed78f732fbdad6cd294ca2e853552ead7b86195
Dmitry Chirikov <dmitry@chirikov.ru>
2016-07-25 21:49:37 +0200

    Drbd config. More checks.

--- configuration/controller/drbd-master.sh
+++ configuration/controller/drbd-master.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 
 function replace_template {
@@ -19,37 +20,32 @@ function replace_template {
 echo_info "Check if config variables are available."
 
 echo "DRBD_DEVICE=${DRBD_DEVICE:?"Should be defined"}"
-echo "DRBD_LOCAL_HOST=${DRBD_LOCAL_HOST:?"Should be defined"}"
+echo "DRBD_LOCAL_IP=${DRBD_LOCAL_IP:?"Should be defined"}"
 DRBD_LOCAL_HOSTNAME=$(hostname)
-echo "DRBD_PARTNER_HOST=${DRBD_PARTNER_HOST:?"Should be defined"}"
+echo "DRBD_PARTNER_IP=${DRBD_PARTNER_IP:?"Should be defined"}"
 echo "DRBD_PATH_TO_MOUNT=${DRBD_PATH_TO_MOUNT:?"Should be defined"}"
 
 echo_info "Check access to remote node."
 
-DRBD_PARTNER_HOSTNAME=`/usr/bin/ssh ${DRBD_PARTNER_HOST} hostname || (echo_error "Unable to connect to ${DRBD_PARTNER_HOST}"; exit 1)`
+DRBD_PARTNER_HOSTNAME=`/usr/bin/ssh ${DRBD_PARTNER_IP} hostname || (echo_error "Unable to connect to ${DRBD_PARTNER_IP}"; exit 1)`
 
 echo_info "Check if block device is available."
 
-(/usr/bin/ssh ${DRBD_PARTNER_HOST} ls ${DRBD_DEVICE} && ls ${DRBD_DEVICE}) || (echo_error "${DRBD_DEVICE} not present on one of the nodes."; exit 2)
+(/usr/bin/ssh ${DRBD_PARTNER_IP} ls ${DRBD_DEVICE} && ls ${DRBD_DEVICE}) || (echo_error "${DRBD_DEVICE} not present on one of the nodes."; exit 2)
 
-echo_info "Copy repo /etc/yum*, /etc/pki and /trinity/packages to remote host."
+echo_info "Check if device is in use."
 
-TMP_TARBALL=$(/usr/bin/mktemp)
-[ "x${TMP_TARBALL}" = "x" ] && (echo_error "Unable to create temp file." ; exit 3)
-pushd /
-/usr/bin/tar -czf ${TMP_TARBALL} etc/yum* etc/pki trinity/packages
-popd
-/usr/bin/scp ${TMP_TARBALL} ${DRBD_PARTNER_HOST}:${TMP_TARBALL}
-/usr/bin/ssh ${DRBD_PARTNER_HOST} "cd / && tar -xzf ${TMP_TARBALL}"
+[ "$(/usr/sbin/blkid ${DRBD_DEVICE})" ] && (echo_error "${DRBD_DEVICE} seems in use."; exit 4)
+[ "$(/usr/bin/ssh ${DRBD_PARTNER_IP} /usr/sbin/blkid ${DRBD_DEVICE})" ] && (echo_error "${DRBD_DEVICE} seems in use on ${DRBD_PARTNER_IP}."; exit 4)
 
-#rm -rf ${TMP_TARBALL}
-#/usr/bin/ssh ${DRBD_PARTNER_HOST} "rm -rf ${TMP_TARBALL}"
+echo_info "Configure firewalld."
 
-echo_info "Install drbd on other host."
-
-/usr/bin/ssh ${DRBD_PARTNER_HOST} "\
-    /usr/bin/yum -y install drbd84-utils kmod-drbd84 \
-"
+if /usr/bin/firewall-cmd --state >/dev/null ; then
+    /usr/bin/firewall-cmd --permanent --add-port=7789/tcp
+    /usr/bin/firewall-cmd --reload
+else
+    echo_warn "Firewalld is not running. 27017/tcp should be open if you enable it later."
+fi
 
 echo_info "Create config."
 
@@ -57,19 +53,19 @@ echo_info "Create config."
 
 /usr/bin/cp ${POST_FILEDIR}/global_common.conf /etc/drbd.d/global_common.conf
 /usr/bin/cp ${POST_FILEDIR}/trinity_disk.res /etc/drbd.d/trinity_disk.res
-for VAR in DRBD_DEVICE DRBD_LOCAL_HOST DRBD_PARTNER_HOST DRBD_LOCAL_HOSTNAME DRBD_PARTNER_HOSTNAME; do
+for VAR in DRBD_DEVICE DRBD_LOCAL_HOSTNAME DRBD_PARTNER_HOSTNAME DRBD_LOCAL_IP DRBD_PARTNER_IP; do
     replace_template $VAR /etc/drbd.d/trinity_disk.res
 done
-/usr/bin/scp  /etc/drbd.d/global_common.conf ${DRBD_PARTNER_HOST}:/etc/drbd.d/global_common.conf
-/usr/bin/scp  /etc/drbd.d/trinity_disk.res ${DRBD_PARTNER_HOST}:/etc/drbd.d/trinity_disk.res
+/usr/bin/scp  /etc/drbd.d/global_common.conf ${DRBD_PARTNER_IP}:/etc/drbd.d/global_common.conf
+/usr/bin/scp  /etc/drbd.d/trinity_disk.res ${DRBD_PARTNER_IP}:/etc/drbd.d/trinity_disk.res
 
 echo_info "Create device."
 
 /usr/sbin/drbdadm create-md trinity_disk
-/usr/bin/ssh ${DRBD_PARTNER_HOST} /usr/sbin/drbdadm create-md trinity_disk
+/usr/bin/ssh ${DRBD_PARTNER_IP} /usr/sbin/drbdadm create-md trinity_disk
 
 /usr/sbin/drbdadm up trinity_disk
-/usr/bin/ssh ${DRBD_PARTNER_HOST} /usr/sbin/drbdadm up trinity_disk
+/usr/bin/ssh ${DRBD_PARTNER_IP} /usr/sbin/drbdadm up trinity_disk
 
 /usr/sbin/drbdadm -- --overwrite-data-of-peer primary trinity_disk
 
@@ -78,7 +74,7 @@ echo_info "Start services."
 systemctl start drbd
 systemctl enable drbd
 
-/usr/bin/ssh ${DRBD_PARTNER_HOST} "systemctl start drbd; systemctl enable drbd"
+/usr/bin/ssh ${DRBD_PARTNER_IP} "systemctl start drbd; systemctl enable drbd"
 
 while [ "x$(/usr/sbin/drbdadm dstate trinity_disk)" != "xUpToDate/UpToDate" ]; do
     echo_info "Waiting for UpToDate/UpToDate"
@@ -93,8 +89,13 @@ echo_info "Format /dev/drdb1"
 [ -d ${DRBD_PATH_TO_MOUNT}.bkp ] && (echo_error "${DRBD_PATH_TO_MOUNT}.bkp exists. Can not proceed.")
 [ -d ${DRBD_PATH_TO_MOUNT} ] && /usr/bin/mv ${DRBD_PATH_TO_MOUNT}{,.bkp}
 
+echo_info "mount /dev/drbd1 as ${DRBD_PATH_TO_MOUNT}"
+
 mkdir ${DRBD_PATH_TO_MOUNT}
 /usr/bin/mount /dev/drbd1 ${DRBD_PATH_TO_MOUNT}
+
+echo_info "Restore data."
+
 pushd ${DRBD_PATH_TO_MOUNT}.bkp
 /usr/bin/tar -cf - . | (cd ${DRBD_PATH_TO_MOUNT} && /usr/bin/tar -xf -)
 popd
