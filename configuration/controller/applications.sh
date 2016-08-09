47e9fb8ac83a1df81272c0dd128a72fd569a2f9a
Hicham Amrati <hicham.amrati@clustervision.com>
2016-08-09 12:58:29 +0200

    Add openmpi/2.0.0 modulefile and dependencies

--- configuration/controller/applications.sh
+++ configuration/controller/applications.sh
@@ -15,22 +15,19 @@
 # This script will replace {{ prefix }} with the correct value
 #
 
-if flag_is_unset CHROOT_INSTALL; then
+for GROUP in 'CV-standard' 'CV-advanced' 'local'; do
 
-    for GROUP in 'CV-standard' 'CV-advanced' 'local'; do
+    echo_info "Adding $GROUP module files to the system"
 
-        echo_info "Adding $GROUP module files to the system"
+    for MOD in $(ls "${POST_FILEDIR}/${GROUP}"); do
 
-        for MOD in $(ls "${POST_FILEDIR}/${GROUP}"); do
+        VERSION=$(ls "${POST_FILEDIR}/${GROUP}/${MOD}");
+        echo_info "Adding ${MOD}/${VERSION} to $GROUP";
 
-            VERSION=$(ls "${POST_FILEDIR}/${GROUP}/${MOD}");
-            echo_info "Adding ${MOD}/${VERSION} to $GROUP";
-
-            cp -r "${POST_FILEDIR}/${GROUP}/${MOD}" "${TRIX_SHARED}/modulefiles/${GROUP}/"
-            sed -i -e "s,{{ prefix }},$TRIX_SHARED," "${TRIX_SHARED}/modulefiles/${GROUP}/${MOD}/${VERSION}/modulefile"
-
-        done
+        cp -r "${POST_FILEDIR}/${GROUP}/${MOD}" "${TRIX_SHARED}/modulefiles/${GROUP}/"
+        sed -i -e "s,{{ prefix }},$TRIX_SHARED," "${TRIX_SHARED}/modulefiles/${GROUP}/${MOD}/${VERSION}/modulefile"
 
     done
 
-fi
+done
+
