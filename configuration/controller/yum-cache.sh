47e7dba6b5c71d927bdeb3de37c2387e14fdc6b2
JF Le Fillâtre <jf.lefillatre@clustervision.com>
2016-08-11 17:22:13 +0200

    Add a flag to disable persistent yum caches
    Actually the whole purpose of the yum-cache PS is that, so disabling it should rather be through commenting out the PS in the POSTLIST.
    
    But as I expect that this PS may become more complex with time, it makes sense to provide an option for that specific item.

--- configuration/controller/yum-cache.sh
+++ configuration/controller/yum-cache.sh
@@ -1,9 +1,17 @@
 
 # Post script to configure yum
 
-echo_info 'Configuring yum to keep all downloaded RPMs'
+display_var YUM_PERSISTENT_CACHE YUM_CLEAR_CACHE
 
-store_system_variable /etc/yum.conf keepcache 1
+
+# Do we want to keep all downloaded RPMs?
+
+if flag_is_set YUM_PERSISTENT_CACHE ; then
+
+    echo_info 'Configuring yum to keep all downloaded RPMs'
+
+    store_system_variable /etc/yum.conf keepcache 1
+fi
 
 
 # Do we need to clear everything first?
