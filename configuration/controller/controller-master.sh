f435eedc02d3d925b7ae65bdd9d29c3f67c49252
Hicham Amrati <hicham.amrati@clustervision.com>
2016-08-25 12:08:46 +0200

    Keep the active controller's IP as is; Assign a new one as FIP
    This basically reverts a previous commit that would transform the
    current IP to a floating IP and gives the machine a new IP.
    
    We are changing this behaviour to prevent potential issues where
    already built images or installed services would require the use
    of the machine IP to function properly rather than the floating IP.

--- configuration/controller/controller-master.sh
+++ configuration/controller/controller-master.sh
@@ -1,59 +1,41 @@
 #!/bin/bash
-set -e
 
-source "$POST_CONFIG"
-source /etc/trinity.sh
-source "$TRIX_SHADOW"
+display_var TRIX_CTRL{1,2,}_{HOSTNAME,IP}
 
-echo "CTRL_IP=${CTRL_IP:?"Should be defined"}"
-CTRL_HOSTNAME=$(/usr/bin/hostname -s)
-CTRL_HOSTNAME_FULL=$(/usr/bin/hostname)
-echo "CTRL1_HOSTNAME_FULL=${CTRL1_HOSTNAME_FULL:?"Should be defined"}"
-CTRL1_HOSTNAME=${CTRL1_HOSTNAME_FULL%%.*}
-echo "CTRL1_IP=${CTRL1_IP:?"Should be defined"}"
-echo "CTRL2_IP=${CTRL2_IP:?"Should be defined"}"
-
-eval $(/usr/bin/ipcalc -np $(/usr/sbin/ip a show to  ${CTRL_IP} | /usr/bin/awk 'NR==2{print $2}') | /usr/bin/awk '{print "CTRL_IP_"$0}')
-echo "CTRL_IP_NETWORK=${CTRL_IP_NETWORK:?"Unable to find"}"
-echo "CTRL_IP_PREFIX=${CTRL_IP_PREFIX:?"Unable to find"}"
+#---------------------------------------
 
 echo_info "Check if remote host is available."
+/usr/bin/ssh ${TRIX_CTRL2_IP} hostname || (echo_error "Unable to connect to ${TRIX_CTRL2_IP}"; exit 1)
 
-CTRL2_HOSTNAME=`/usr/bin/ssh ${CTRL2_IP} hostname -s || (echo_error "Unable to connect to ${CTRL2_IP}"; exit 1)`
-CTRL2_HOSTNAME_FULL=`/usr/bin/ssh ${CTRL2_IP} hostname || (echo_error "Unable to connect to ${CTRL2_IP}"; exit 1)`
+#---------------------------------------
 
-echo_info "Add records to /etc/hosts"
+echo_info "Copy /etc/hosts to ${TRIX_CTRL2_HOSTNAME}"
 
-cat <<EOF >>/etc/hosts
-# --- Trinity HA config controller-master.sh ---
-${CTRL_IP}      ${CTRL_HOSTNAME} ${CTRL_HOSTNAME_FULL}
-${CTRL1_IP}     ${CTRL1_HOSTNAME} ${CTRL1_HOSTNAME_FULL}
-${CTRL2_IP}     ${CTRL2_HOSTNAME} ${CTRL2_HOSTNAME_FULL}
-EOF
+/usr/bin/scp /etc/hosts ${TRIX_CTRL2_IP}:/etc/hosts
 
-echo_info "Copy /etc/hosts to ${CTRL2_IP}"
+#---------------------------------------
 
-/usr/bin/scp /etc/hosts ${CTRL2_IP}:/etc/hosts
+# Add floating IP to this controller if it is not present already
 
-echo_info "Find out which interface owns ${CTRL_IP}"
+if [[ -z $(ip addr show to $TRIX_CTRL_IP) ]]; then
 
-TMPVAR=$(/usr/sbin/ip a show to ${CTRL_IP} | awk 'NR==1{print $2}')
-CTRL1_IF=${TMPVAR%:}
-echo "${CTRL1_IF:?"Error in finding interface."}"
+    echo_info "Find out which interface owns ${TRIX_CTRL1_IP}"
+    
+    TMPVAR=$(/usr/sbin/ip a show to ${TRIX_CTRL1_IP} | awk 'NR==1{print $2}')
+    CTRL1_IF=${TMPVAR%:}
+    
+    echo "${CTRL1_IF:?"Could not find interface."}"
+    
+    echo_info "Assign floating IP"
+    
+    eval $(/usr/bin/ipcalc -np $(/usr/sbin/ip a show to  ${TRIX_CTRL1_IP} | /usr/bin/awk 'NR==2{print $2}') | /usr/bin/awk '{print "CTRL_IP_"$0}')
+    echo "CTRL_IP_PREFIX=${CTRL_IP_PREFIX:?"Unable to find"}"
 
-echo_info "Reconfigure interface ifcfg-${CTRL1_IF}"
+    /usr/sbin/ip a add ${TRIX_CTRL_IP}/${CTRL_IP_PREFIX} dev ${CTRL1_IF}
 
-/usr/bin/sed -i -e "s/IPADDR=${CTRL_IP}/IPADDR=${CTRL1_IP}/" /etc/sysconfig/network-scripts/ifcfg-${CTRL1_IF}
+else
 
-echo_info "Reset interface"
+    echo_info "Floating IP already assigned. Skipping."
 
-/usr/sbin/ifdown ${CTRL1_IF}
-/usr/sbin/ifup ${CTRL1_IF}
+fi
 
-echo_info "Assign alias"
-
-/usr/sbin/ip a add ${CTRL_IP}/${CTRL_IP_PREFIX} dev ${CTRL1_IF}
-
-echo_info "Change hostname from ${CTRL_HOSTNAME} to ${CTRL1_HOSTNAME}"
-
-/usr/bin/hostnamectl set-hostname ${CTRL1_HOSTNAME_FULL}
