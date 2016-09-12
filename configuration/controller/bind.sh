b9096586e3d84b2b37f0e0ddff7d021c98842308
JF Le Fillâtre <jf.lefillatre@clustervision.com>
2016-09-12 22:58:37 +0200

    BIND: add option to define static forwarders

--- configuration/controller/bind.sh
+++ configuration/controller/bind.sh
@@ -10,10 +10,6 @@ sed -i -e 's/\(.*listen-on port 53 { \).*\( };\)/\1any;\2/' /etc/named.conf
 echo_info 'Make named accept queries from all nodes that are not blocked by the firewall'
 sed -i -e 's,\(.*allow-query\s.*{ \).*\( };\),\1any;\2,' /etc/named.conf
 
-echo_info 'Enable and start named service'
-systemctl enable named
-systemctl start named
-
 
 echo_info "Use this DNS server as the default resolver"
 
@@ -32,4 +28,29 @@ done
 echo_info 'Setting up dhclient to avoid overwriting our configuration'
 
 cp "${POST_FILEDIR}/dhclient-enter-hooks" /etc/dhcp
+
+
+if flag_is_set BIND_FORWARDERS ; then
+    echo_info 'Setting up the DNS forwarders'
+
+    # Create a forwarders files that we will include in the main config file
+    (
+    echo '// ICS BIND DNS forwarders'
+    echo 'forwarders {'
+    for i in $BIND_FORWARDERS ; do
+        echo -e "\t${i};"
+    done
+    echo -e '};'
+    ) | tee /etc/named.forwarders.conf
+
+    # And include it
+    if ! grep -q /etc/named.forwarders.conf /etc/named.conf ; then
+        sed -i '/recursion yes/a \\tinclude "/etc/named.forwarders.conf";' /etc/named.conf
+    fi
+fi
+
+
+echo_info 'Enabling and starting the named service'
+systemctl enable named
+systemctl start named
 
