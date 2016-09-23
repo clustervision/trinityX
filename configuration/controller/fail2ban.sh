e79ac11705c3a9641ec167bd09e9d1c1a421144a
JF Le Fillâtre <jf.lefillatre@clustervision.com>
2016-09-23 14:07:02 +0200

    Tweak fail2ban for use in images

--- configuration/controller/fail2ban.sh
+++ configuration/controller/fail2ban.sh
@@ -8,5 +8,5 @@ cp "${POST_FILEDIR}"/trinityx.conf /etc/fail2ban/jail.d/
 echo_info 'Enabling and starting fail2ban'
 
 systemctl enable fail2ban
-systemctl restart fail2ban
+flag_is_unset POST_CHROOT && systemctl restart fail2ban || true
 
