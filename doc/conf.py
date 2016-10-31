cccffc375ebc58ba282beac541879682859f762c
JF Le Fillâtre <jf.lefillatre@clustervision.com>
2016-10-31 11:50:53 +0100

    [doc] Use the standard CV logo for now

--- doc/conf.py
+++ doc/conf.py
@@ -146,7 +146,7 @@ html_title = 'TrinityX r10'
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 #
-html_logo = 'CV_monochromatic_portrait_white.png'
+html_logo = 'CV_monochromatic_portrait_white_small.png'
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
