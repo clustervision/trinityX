9c6ee2991a95c955013d29b1a85f4250ef5af1f0
Dmitry Chirikov <dmitry@chirikov.ru>
2016-11-18 15:38:05 +0100

    License headers.

--- scripts/ldapbackup.sh
+++ scripts/ldapbackup.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+
+######################################################################
+# TrinityX
+# Copyright (c) 2016  ClusterVision B.V.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License (included with the sources) for more
+# details.
+######################################################################
+
 set -e
 trap cleanup EXIT
 trap report_err ERR
@@ -21,7 +38,7 @@ mkdir ${BKPDIR}
 pushd ${BKPDIR} > /dev/null
 slapcat -b cn=config | \
     awk -F\= '/^dn: olcDatabase=/{gsub("{|}|,"," ", $2);print $2}' | \
-        while read INDEX r; do 
+        while read INDEX r; do
             if [ ${INDEX} -ge 0 ]; then
                 echo "INFO: Backup DB index ${INDEX}"
                 slapcat -n ${INDEX} -l ldap-olcdatabase-${INDEX}.bkp.ldif || \
