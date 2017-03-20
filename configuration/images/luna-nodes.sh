30fbfb86d096168049121c573217b9363a0c0cf7
Dmitry Chirikov <dmitry@chirikov.ru>
2017-03-20 13:25:29 +0100

    Yum versionlock for luna-client

--- configuration/images/luna-nodes.sh
+++ configuration/images/luna-nodes.sh
@@ -3,12 +3,12 @@
 ######################################################################
 # TrinityX
 # Copyright (c) 2016  ClusterVision B.V.
-# 
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -16,15 +16,6 @@
 # details.
 ######################################################################
 
+echo_info 'yum version lock for luna.'
 
-# Copy luna's dracut module to the image. The files are supposed to be located at
-# ${TRIX_LOCAL}/luna/dracut/95luna
-
-echo_info 'Installing luna dracut module'
-
-if [[ -d "${TRIX_LOCAL}/luna/dracut/95luna" ]]; then
-    cp -pr "${TRIX_LOCAL}/luna/dracut/95luna" "/usr/lib/dracut/modules.d/"
-else
-    echo_error 'Could not find the dracut module in ${TRIX_LOCAL}/luna/dracut/95luna'
-fi
-
+/usr/bin/yum versionlock luna-client-*
