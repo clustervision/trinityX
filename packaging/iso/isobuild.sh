8301db712b36e1660e9ad922416616cddcaaaa5f
Dmitry Chirikov <dmitry@chirikov.ru>
2018-07-04 12:10:30 +0200

    Building mimimal ISO

--- packaging/iso/isobuild.sh
+++ packaging/iso/isobuild.sh
@@ -4,8 +4,12 @@ set -e
 set -x
 
 # To download packages
-# Fomr one of the controlelrs in HA config
+# From one of the controlelrs in HA config
 # mkdir -p /root/Packages; rpm -qa | paste -d " " $(printf " -%.0s" {1..50}) | while read L; do yumdownloader --enablerepo="elrepo" --destdir /root/Packages ${L}; done
+# for G in base core debugging development hardware-monitoring network-tools performance system-admin-tools system-management; do
+#    yumdownloader @${G} --setopt=group_package_types=mandatory,default,optional --resolve --destdir /root/Packages
+# done
+#
 
 CENTOS_CONTENT=$1
 TRIX_PACKAGES=$2
@@ -38,33 +42,26 @@ SCRIPTDIR=$(
 
 ISO_DIR=${SCRIPTDIR}/ISO
 
-mkdir -p ${ISO_DIR}
+mkdir -p ${ISO_DIR}/Packages
 
-cp -pr ${CENTOS_CONTENT}/{EFI,GPL,images,isolinux,Packages,LiveOS} ${ISO_DIR}/
+cp -pr ${CENTOS_CONTENT}/{EFI,GPL,images,isolinux,LiveOS} ${ISO_DIR}/
 
 cp -pr ${TRIX_PACKAGES}/* ${ISO_DIR}/Packages/
-xsltproc --novalid ${SCRIPTDIR}/trinity-comps.xsl ${CENTOS_CONTENT}/repodata/*-comps.xml > ${ISO_DIR}/comps.xml
+echo '<?xml version="1.0" encoding="UTF-8"?>' > ${ISO_DIR}/comps.xml
+echo '<!DOCTYPE comps PUBLIC "-//CentOS//DTD Comps info//EN" "comps.dtd">' >> ${ISO_DIR}/comps.xml
+echo '<comps>' >> ${ISO_DIR}/comps.xml
+xsltproc \
+    --novalid ${SCRIPTDIR}/trinity-comps.xsl \
+    ${CENTOS_CONTENT}/repodata/*-comps.xml \
+    | grep -v '<?xml version="1.0"?>' >> ${ISO_DIR}/comps.xml
+cat ${SCRIPTDIR}/trinity-comps.xml >> ${ISO_DIR}/comps.xml
+echo '</comps>' >> ${ISO_DIR}/comps.xml
 
 pushd ${ISO_DIR}
 createrepo --groupfile ./comps.xml .
+repoclosure --repofrompath=0,. -r 0
+rmdir 0
 popd
-
-#mkdir ${SCRIPTDIR}/tmp-LiveOS
-
-#pushd ${SCRIPTDIR}/tmp-LiveOS
-#unsquashfs ${CENTOS_CONTENT}/LiveOS/squashfs.img
-#mkdir rootfs
-#mount squashfs-root/LiveOS/rootfs.img rootfs
-#cp -pr ${SCRIPTDIR}/anaconda/* rootfs/usr/share/anaconda/
-#umount rootfs
-#popd
-
-#mksquashfs ${SCRIPTDIR}/tmp-LiveOS/squashfs-root/LiveOS ${ISO_DIR}/LiveOS/squashfs.img -comp xz -keep-as-directory
-
-#rm -rf ${SCRIPTDIR}/tmp-LiveOS
-
-
-#mkisofs -o ${SCRIPTDIR}/${ISO_NAME} -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot  -V "CentOS 7 x86_64" -boot-load-size 4 -boot-info-table -R -J -joliet-long -v -T ${ISO_DIR}
 
 pushd ${SCRIPTDIR}/product
 find . | cpio -c -o | gzip -9cv > ${ISO_DIR}/images/product.img
