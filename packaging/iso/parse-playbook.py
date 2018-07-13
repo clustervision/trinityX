ed1077c7e31722814b49aa3e564b01f1ed80898c
Dmitry Chirikov <dmitry@chirikov.ru>
2018-07-13 20:08:42 +0200

    Add exit code and show errors to stderr

--- packaging/iso/parse-playbook.py
+++ packaging/iso/parse-playbook.py
@@ -134,4 +134,8 @@ def main():
         print p
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except:
+        sys.stderr.write(traceback.format_exc(limit=10))
+        sys.exit(1)
