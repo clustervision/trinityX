dea7efa16690a0152af623b89e09e550ae7466b6
root <root@rhel11.cluster>
2021-02-16 11:41:16 +0100

    switched from zip to tgz and from wget to curl

--- prepare.sh
+++ prepare.sh
@@ -6,14 +6,14 @@ else
   yum update -y
   curl https://updates.clustervision.com/luna/1.2/centos/luna-1.2.repo > /etc/yum.repos.d/luna-1.2.repo
   yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
-  yum install ansible git luna-ansible python-pip -y
+  yum install curl tar ansible git luna-ansible python-pip -y
   ansible-galaxy install OndrejHome.pcs-modules-2
   if [ ! -d site/mitogen ]
   then
-    wget https://github.com/mitogen-hq/mitogen/archive/v0.2.9.zip
-    unzip v0.2.9.zip
+    curl https://codeload.github.com/mitogen-hq/mitogen/tar.gz/v0.2.9 --output mitogen.tar.gz
+    tar -zxf mitogen.tar.gz
     mv mitogen-0.2.9 site/mitogen
-    rm -f v0.2.9.zip
+    rm -f mitogen.tar.gz
   fi
   echo "#### Please configure the network before starting Ansible ####"
 fi
