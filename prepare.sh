a5e2417bc35aa1dcb71aa14b0218011358bc40d6
bart <bart@sandbox01.clustervision.lan>
2021-02-16 11:16:12 +0100

    removed mitogen git checkout and added mitogen release

--- prepare.sh
+++ prepare.sh
@@ -8,6 +8,12 @@ else
   yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
   yum install ansible git luna-ansible python-pip -y
   ansible-galaxy install OndrejHome.pcs-modules-2
-  git clone https://github.com/dw/mitogen.git site/mitogen
+  if [ ! -d site/mitogen ]
+  then
+    wget https://github.com/mitogen-hq/mitogen/archive/v0.2.9.zip
+    unzip v0.2.9.zip
+    mv mitogen-0.2.9 site/mitogen
+    rm -f v0.2.9.zip
+  fi
   echo "#### Please configure the network before starting Ansible ####"
 fi
