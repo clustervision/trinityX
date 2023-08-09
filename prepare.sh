59b435a0da99098d70ae23c7c37f0661e90e2fae
antoine <antoine.schonewille@clustervision.com>
2023-08-09 12:58:50 +0200

    NOJIRA i am certain that i broke things but this is a first attempt to merge Bart's repo with the new one

--- prepare.sh
+++ prepare.sh
@@ -4,16 +4,8 @@ if [ ! -f TrinityX.pdf ]; then
 else
   setenforce 0
   yum update -y
-  curl https://updates.clustervision.com/luna/1.2/centos/luna-1.2.repo > /etc/yum.repos.d/luna-1.2.repo
-  yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
-  yum install curl tar ansible git luna-ansible python-pip -y
+  yum install epel-release -y
+  yum install curl tar ansible git python2-pip epel-release -y
   ansible-galaxy install OndrejHome.pcs-modules-2
-  if [ ! -d site/mitogen ]
-  then
-    curl https://codeload.github.com/mitogen-hq/mitogen/tar.gz/v0.2.9 --output mitogen.tar.gz
-    tar -zxf mitogen.tar.gz
-    mv mitogen-0.2.9 site/mitogen
-    rm -f mitogen.tar.gz
-  fi
   echo "#### Please configure the network before starting Ansible ####"
 fi
