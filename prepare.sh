45ddec4e00f6528e7deaa5086613a7e54a718271
antoine <antoine.schonewille@clustervision.com>
2023-08-09 15:09:45 +0200

    NOJIRA more diffs incorporated

--- prepare.sh
+++ prepare.sh
@@ -5,7 +5,7 @@ else
   setenforce 0
   yum update -y
   yum install epel-release -y
-  yum install curl tar ansible git python2-pip epel-release -y
+  yum install curl tar ansible git epel-release -y
   ansible-galaxy install OndrejHome.pcs-modules-2
   echo "#### Please configure the network before starting Ansible ####"
 fi
