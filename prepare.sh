054746c0f7a2af95ea9ccf42f7b80c8780eacf3c
antoine <antoine.schonewille@clustervision.com>
2023-09-05 14:51:07 +0200

    NOTRIX moving yum/dns clean all back from prepare

--- prepare.sh
+++ prepare.sh
@@ -8,5 +8,5 @@ else
   yum install curl tar ansible git epel-release -y
 #  ansible-galaxy install OndrejHome.pcs-modules-2
   echo "#### Please configure the network before starting Ansible ####"
-  dnf clean all
+#  dnf clean all
 fi
