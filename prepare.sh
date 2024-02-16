cd3a1db3a9cfaaaaa4bab296deb79e5f1ef5407f
antoine <antoine.schonewille@clustervision.com>
2024-02-16 10:42:51 +0100

    NOJIRA testing with less options

--- prepare.sh
+++ prepare.sh
@@ -4,9 +4,6 @@ if [[ `getenforce` == "Disabled" ]]; then
     if [[ `grep "^SELINUX=disabled$" /etc/selinux/config` ]]; then
         sed -i 's/SELINUX=disabled/SELINUX=permissive/g' /etc/selinux/config
         exit 1
-    else
-        sed -i 's/SELINUX=enforcing/SELINUX=permissive/g' /etc/selinux/config
-        exit 1
     fi
 fi
 if [ ! -f TrinityX.pdf ]; then
@@ -19,11 +16,11 @@ else
   yum update -y
   yum install curl tar git -y
 
-  if  [[ `grep -i "PRETTY_NAME=\"Red Hat Enterprise Linux 8" /etc/os-release` ]]; then
+  if  [[ `grep -i "Red Hat Enterprise Linux 8" /etc/os-release` ]]; then
     subscription-manager repos --enable codeready-builder-for-rhel-8-x86_64-rpms
     yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
     yum install ansible-core -y
-  elif  [[ `grep -i "PRETTY_NAME=\"Red Hat Enterprise Linux 9" /etc/os-release` ]]; then
+  elif  [[ `grep -i "Red Hat Enterprise Linux 9" /etc/os-release` ]]; then
     subscription-manager repos --enable codeready-builder-for-rhel-9-x86_64-rpms
     yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm -y
     yum install ansible-core -y
@@ -33,11 +30,11 @@ else
   fi
 
   ansible-galaxy install OndrejHome.pcs-modules-2
-  ansible-galaxy collection install ansible.posix
-  ansible-galaxy collection install community.general
-  ansible-galaxy collection install community.mysql
-  ansible-galaxy collection install community.rabbitmq
-  ansible-galaxy collection install community.grafana
+#  ansible-galaxy collection install ansible.posix
+#  ansible-galaxy collection install community.general
+#  ansible-galaxy collection install community.mysql
+#  ansible-galaxy collection install community.rabbitmq
+#  ansible-galaxy collection install community.grafana
 
   if [ ! -f site/hosts ]; then
     echo "Please modify the site/hosts.example and save it as site/hosts"  
