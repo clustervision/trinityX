b1c16471ce357b05b6812909bf461281f64bd3a1
root <root@controller3-twan.cv.taurusgroup.one>
2024-03-13 18:07:01 +0100

    TRIX-393 lvm fix script implemented

--- site/roles/trinity/shared-fs/files/lvm-fixes.sh
+++ site/roles/trinity/shared-fs/files/lvm-fixes.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-LVM_CONF=/etc/lvm/lvm.conf_
+LVM_CONF=/etc/lvm/lvm.conf
 
-LVM_DISKS=$(cat /tmp/lvm-disks.dat | grep -v '^#')
+LVM_DISKS=$(cat /tmp/lvm_filter.dat | grep -v '^#')
 find /dev -type l -exec echo -n {}'=' \; -exec readlink -f {} \; > /tmp/dev-links.dat
 
 FILTER="["
@@ -35,10 +35,10 @@ FILTER="$FILTER \"a|.*|\" ]"
 echo "filter = $FILTER"
 
 VOLUMES="["
-VGROUPS=$(cat /tmp/vgroups.dat | grep -v '^#' )
+VGROUPS=$(cat /tmp/lvm_volumes.dat | grep -v '^#' )
 if [ "$VGROUPS" ]; then
 	VGROUPS=$(echo $VGROUPS | sed -e 's/ / -e /g')
-	VGS=$(vgs | tail -n+2 | awk '{ print $1 }' | grep -v $VGROUPS)
+	VGS=$(vgs | grep -v arning | tail -n+2 | awk '{ print $1 }' | grep -v -e $VGROUPS)
 	for V in $VGS; do
 		VOLUMES="$VOLUMES \"$V\","
 	done
@@ -48,19 +48,26 @@ VOLUMES="$VOLUMES ]"
 
 echo "volume_list = $VOLUMES"
 
+DEVICE_PRESENT=$(grep '^\s*use_devicesfile\s*=' $LVM_CONF)
 FILTER_PRESENT=$(grep '^\s*filter\s*=' $LVM_CONF)
 VOLUMES_PRESENT=$(grep '^\s*volume_list\s*=' $LVM_CONF)
 
+if [ "$DEVICE_PRESENT" ]; then
+	sed -i "s%^\(\s\+use_devicesfile\s\+?=.*\)$%# TRINITYX \1\n\tuse_devicesfile = 0%g" $LVM_CONF
+else
+	echo -e "\tuse_devicesfile = 0" > /tmp/__lvm_device.dat
+	sed -i '/^devices {/r /tmp/__lvm_device.dat' $LVM_CONF
+fi
 if [ "$FILTER_PRESENT" ]; then
 	sed -i "s%^\(\s\+filter\s\+?=.*\)$%# TRINITYX \1\n\tfilter = $FILTER%g" $LVM_CONF
 else
-	echo -e "\tfilter = $FILTER" > /tmp/lvm_filter.dat
-	sed -i '/^devices {/r /tmp/lvm_filter.dat' $LVM_CONF
+	echo -e "\tfilter = $FILTER" > /tmp/__lvm_filter.dat
+	sed -i '/^devices {/r /tmp/__lvm_filter.dat' $LVM_CONF
 fi
 if [ "$VOLUMES_PRESENT" ]; then
 	sed -i "s%^\(\s\+volume_list\s\+=.*\)%# TRINITYX \1\n\tvolume_list = $VOLUMES%g" $LVM_CONF
 else
-	echo -e "\tvolume_list = $VOLUMES" > /tmp/lvm_volumes.dat
-	sed -i '/^activation {/r /tmp/lvm_volumes.dat' $LVM_CONF
+	echo -e "\tvolume_list = $VOLUMES" > /tmp/__lvm_volumes.dat
+	sed -i '/^activation {/r /tmp/__lvm_volumes.dat' $LVM_CONF
 fi
 
