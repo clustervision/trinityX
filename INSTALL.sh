d172aa1d0164c73c91316eea21b64919aea883ad
antoine <antoine.schonewille@clustervision.com>
2024-05-29 13:19:56 +0200

    polishing the scripts

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
+function add_message() {
+  echo -e "$1" | fold -w70 -s >> /tmp/mesg.$$.dat
+}
+
+function show_message() {
+  echo "+---------------------------------- X -------------------------------------+"
+  echo "|                                                                          |"
+  while read -r LINE
+  do
+    printf '|  %-70s  |\n' "$LINE"
+  done < /tmp/mesg.$$.dat
+  echo "|                                                                          |"
+  echo "+--------------------------------------------------------------------------+"
+  truncate -s0 /tmp/mesg.$$.dat
+}
+
 echo
-echo "+------------------------------------------------------------+"
-echo "|  Welcome to TrinityX                                       |"
-echo "|  The system will now be prepared first...                  |"
-echo "+------------------------------------------------------------+"
+add_message "Welcome to TrinityX"
+add_message "The system will now be prepared first..."
+show_message
+echo
 sleep 5
 bash prepare.sh
 cd site
@@ -14,8 +30,7 @@ if [ ! "$(grep 'yml check' group_vars/all.yml)" ]; then
 fi
 cp hosts.example hosts
 echo
-echo "+------------------------------------------------------------+"
-echo "|  Ansible Playbooks will now run                            |"
-echo "+------------------------------------------------------------+"
+add_message "Ansible Playbooks will now run"
+show_message
 sleep 5
 ansible-playbook controller.yml compute*.yml
